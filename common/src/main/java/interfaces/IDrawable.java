26d180114f9cb9234bdde4c1b7e6097f333af21c
Sindahlof <frsin21@student.sdu.dk>
2023-03-31 16:21:56 +0200

    half implemented player, will be worked on more later

--- common/src/main/java/interfaces/IDrawable.java
+++ common/src/main/java/interfaces/IDrawable.java
@@ -1,5 +1,5 @@
 package interfaces;
 
 public interface IDrawable {
-    public void draw();
+    void draw(int posx, int posY);
 }
---
+++ player/src/main/java/PlayerProcces.java
@@ -0,0 +1,12 @@
+import interfaces.IMovement;
+import interfaces.IProcessing;
+
+public class PlayerProcces implements IProcessing, IMovement {
+    @Override
+    public void process() {
+
+    }
+
+
+
+}
