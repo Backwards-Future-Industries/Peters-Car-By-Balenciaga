7b977f354998242e2a64483e865e257191a44e22
DominicNr2 <dongu21@student.sdu.dk>
2023-04-09 14:15:12 +0200

    made a bullet class

--- bullet/src/main/java/bullet.java
+++ bullet/src/main/java/bullet.java
@@ -1,2 +1,16 @@
-public class bullet {
+import abstractClasses.Entity;
+import abstractClasses.Weapon;
+
+import java.awt.image.BufferedImage;
+
+public class bullet extends Weapon {
+
+    public bullet(int health, BufferedImage sprite) {
+        super(health, sprite);
+    }
+
+    @Override
+    public Entity shot() {
+        return null;
+    }
 }
