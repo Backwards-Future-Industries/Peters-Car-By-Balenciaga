8b5f4eeb69b72b786cfe6fade87146d585b37d1a
PeterXRiber <perib21@student.sdu.dk>
2023-04-09 12:34:59 +0200

    git commit
    Committing changes to the pom-files

---
+++ map/src/main/java/MapPlugin.java
@@ -0,0 +1,24 @@
+public class MapPlugin {
+    private int width = 10;
+    private int height = 10;
+    private Tile[][] tiles;
+
+
+    public MapPlugin(int width, int height) {
+        this.width = width;
+        this.height = height;
+        tiles = new Tile[width][height];
+
+        // Fills the map with tiles
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = new Tile(x,y);
+            }
+        }
+    }
+
+
+
+
+
+}
---
+++ map/src/main/java/Tile.java
@@ -0,0 +1,27 @@
+
+public class Tile {
+    private int x;
+    private int y;
+    private String type;
+
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+        type = randomTileType();
+    }
+
+    // getter for the tile's type
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String randomTileType() {
+        String[] types = {"Road", "Grass", "Mountain"};
+        int randomIndex = (int) (Math.random() * types.length);
+        return types[randomIndex];
+    }
+}
