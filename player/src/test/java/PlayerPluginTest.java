5a81e0b5024d7c839bd3f463d873acbe63c8ca18
Emre <emeke21@student.sdu.dk>
2023-04-10 12:20:06 +0200

    fixed player so its better

--- player/src/test/java/PlayerPluginTest.java
+++ player/src/test/java/PlayerPluginTest.java
@@ -5,24 +5,26 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class PlayerPluginTest {
     private PlayerPlugin playerPlugin;
-    private double[] list;
+
 
     @BeforeEach
     void setPlayerPlugin(){
-        this.playerPlugin = new PlayerPlugin();
-        this.list = new double[2];
-        this.list[0] = 10;
-        this.list[1] = 10;
-
+        try {
+            this.playerPlugin = new PlayerPlugin();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     public void testReturnType(){
-        Assertions.assertEquals(this.list[0],playerPlugin.create().getPosition()[0]);
-        Assertions.assertEquals(this.list[1],playerPlugin.create().getPosition()[1]);
+
+        Assertions.assertEquals(10,playerPlugin.getPosition()[0]);
+        Assertions.assertEquals(10,playerPlugin.getPosition()[1]);
     }
 }
 
