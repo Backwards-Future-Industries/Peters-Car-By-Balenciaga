cb4baf499698182c3965f07edde6e39a89e9c8d4
Sindahlof <frsin21@student.sdu.dk>
2023-04-26 14:06:59 +0200

    updated SPIlocator comment for easier use of class

--- common/src/main/java/utilities/SPIlocator.java
+++ common/src/main/java/utilities/SPIlocator.java
@@ -11,10 +11,16 @@ public class SPIlocator {
     All places where the SPIlocator is needed, create a new method like down below
 
     public Collection<IPlugin> getPlugin(){
-        return SPILocater.localteAll(IPlugin.class);
+        return SPIlocator.locateAll(IPlugin.class);
     }
 
     Remeber to make it a collection that contains the Interface you want to call to access it's methods
+
+    Below a for each loop can be seen, remember to make for each iterate over the datatype wich methods you want to use
+
+    for (IPlugin iPlugin : getPlugin()){
+            iPlugin.create();
+        }
      */
 
 
