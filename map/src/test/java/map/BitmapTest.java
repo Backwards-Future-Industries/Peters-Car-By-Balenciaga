79e1d165c0ca0f33a341ea87a04615db15bd8f5a
PeterXRiber <perib21@student.sdu.dk>
2023-04-28 14:21:25 +0200

    Update BitmapTest.java
    Just added some test methods

--- map/src/test/java/map/BitmapTest.java
+++ map/src/test/java/map/BitmapTest.java
@@ -4,19 +4,26 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BitmapTest {
 
     @BeforeEach
     void setUp() {
+       
+
+
     }
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
     void getMap() {
+
     }
 }
