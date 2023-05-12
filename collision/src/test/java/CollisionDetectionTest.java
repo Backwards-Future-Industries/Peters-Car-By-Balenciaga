27eeb59c11fa47d5f5cef06f5c839bb1bdd66e68
OFHansen <ohans21@student.sdu.dk>
2023-05-12 13:45:13 +0200

    Changed from pythagoras to box collision

--- collision/src/test/java/CollisionDetectionTest.java
+++ collision/src/test/java/CollisionDetectionTest.java
@@ -28,16 +28,16 @@ class CollisionDetectionTest {
 
 
     @Test
-    public void notColliding(){
-        testEntity1.setPosition(new int[]{400,400});
-        testEntity2.setPosition(new int[]{400,440});
-        Assertions.assertFalse(collisionDetection.isColliding(testEntity1,testEntity2));
+    void testCollisionDetection() {
+        testEntity1.setPosition(new int[]{0, 0});
+        testEntity2.setPosition(new int[]{479, 0});
+        Assertions.assertTrue(collisionDetection.isColliding(testEntity1, testEntity2));
     }
 
     @Test
     void testNoCollision() {
         testEntity1.setPosition(new int[]{0, 0});
-        testEntity2.setPosition(new int[]{960, 0});
+        testEntity2.setPosition(new int[]{480, 0});
         Assertions.assertFalse(collisionDetection.isColliding(testEntity1, testEntity2));
     }
 
