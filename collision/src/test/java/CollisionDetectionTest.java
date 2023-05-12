52a71947eb87451388e402b2d74fe09e4772ae3b
OFHansen <ohans21@student.sdu.dk>
2023-05-12 07:18:49 +0200

    Will write tests for the other method and adjust current tests once we have established a standard in regards to scaling

--- collision/src/test/java/CollisionDetectionTest.java
+++ collision/src/test/java/CollisionDetectionTest.java
@@ -9,6 +9,8 @@ import java.net.URL;
 class CollisionDetectionTest {
 
     CollisionDetection collisionDetection = new CollisionDetection();
+
+    //This placeholder sprite is 480x288
     private static URL sprite = TestEntity.class.getClassLoader().getResource("images/placeholder.png");
     private class TestEntity extends Entity {
 
@@ -21,7 +23,18 @@ class CollisionDetectionTest {
     TestEntity testEntity1 = new TestEntity(1,sprite);
     TestEntity testEntity2 = new TestEntity(1,sprite);
 
+    @Test
+    void testCollisionDetection() {
+        testEntity1.setPosition(new int[]{0, 0});
+        testEntity2.setPosition(new int[]{959, 0});
+        Assertions.assertTrue(collisionDetection.isColliding(testEntity1, testEntity2));
+    }
 
-
+    @Test
+    void testNoCollision() {
+        testEntity1.setPosition(new int[]{0, 0});
+        testEntity2.setPosition(new int[]{960, 0});
+        Assertions.assertFalse(collisionDetection.isColliding(testEntity1, testEntity2));
+    }
 
 }
