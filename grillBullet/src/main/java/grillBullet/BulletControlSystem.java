280defe1ad3772fd9d153abe26b1548e38060fb6
Sindahlof <90039730+Sindahlof@users.noreply.github.com>
2023-05-15 13:52:09 +0200

    Update BulletControlSystem.java

--- grillBullet/src/main/java/grillBullet/BulletControlSystem.java
+++ grillBullet/src/main/java/grillBullet/BulletControlSystem.java
@@ -13,9 +13,12 @@ public class BulletControlSystem implements IProcessing {
     @Override
     public void process(ArrayList<Inputs> inputs, GameData gameData) {
         for (Entity bullet : gameData.getEntityList(Type.BULLET)) {
-            if (bullet.getType() == Type.BULLET) {
-                //for (IMovement iMovement : getIPlugin()) {}}}
-            }
+            for (IMovement iMovement : getIPlugin()) {
+                    bullet.setPosition(iMovement.defaultMove(new ArrayList<>(List.of(KEY_W)), bullet));
+                }   
         }
+    }
+     private Collection<IMovement> getIPlugin() {
+        return SPIlocator.locateAll(IMovement.class);
     }
 }
