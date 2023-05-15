77753124fae24d2e9318d5786cdc4e939a3ad079
Eivan20 <eivan20@student.sdu.dk>
2023-05-15 14:37:45 +0200

    Fixed the map rotation

--- map/src/main/java/map/Map.java
+++ map/src/main/java/map/Map.java
@@ -22,7 +22,7 @@ public class Map extends Entity implements IDrawable, IPlugin {
     private Tile earth;
     private Tile obstacle;
 
-    private ArrayList <Shapes> shapesArray;
+    private ArrayList<Shapes> shapesArray;
 
     // combinedTiles:
     // Combines the buffered images into one image, which is then drawn in the draw-method
@@ -31,40 +31,39 @@ public class Map extends Entity implements IDrawable, IPlugin {
     // so the tiles/shapes can be defined as obstacles, roads ect. and collision control can be performed on the map.
     private void combinedTiles() {
 
-        BufferedImage bufferedImage = new BufferedImage(bitmap.getMap().length*16,
-                bitmap.getMap()[0].length*16,
+        BufferedImage bufferedImage = new BufferedImage(bitmap.getMap().length * 16,
+                bitmap.getMap()[0].length * 16,
                 BufferedImage.TYPE_3BYTE_BGR);
         Graphics2D g = bufferedImage.createGraphics();
         TileType[][] map = bitmap.getMap();
 
-        int[] position = new int[]{0,0};
+        int[] position = new int[]{0, 0};
 
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
-                TileType tileType = map[x][y];
-                if(tileType == TileType.EARTH){
-                    g.drawImage(earth.getSprite().getImage(),position[0],position[1],null);
+                TileType tileType = map[y][x];
+                if (tileType == TileType.EARTH) {
+                    g.drawImage(earth.getSprite().getImage(), position[0], position[1], null);
                 }
-                if(tileType == TileType.GRASS){
-                    g.drawImage(grass.getSprite().getImage(),position[0],position[1],null);
+                if (tileType == TileType.GRASS) {
+                    g.drawImage(grass.getSprite().getImage(), position[0], position[1], null);
                 }
-                if(tileType == TileType.OBSTACLE){
-                    g.drawImage(obstacle.getSprite().getImage(),position[0],position[1],null);
-                }
-                position = new int[]{position[0]+16,position[1]};
                 if (tileType == TileType.OBSTACLE) {
-                    shapesArray.add(new Shapes(16,16,position, Types.OBSTACLE));
+                    g.drawImage(obstacle.getSprite().getImage(), position[0], position[1], null);
                 }
-                else {
-                    shapesArray.add(new Shapes(16,16,position, Types.UNDEFINED));
+                if (tileType == TileType.OBSTACLE) {
+                    shapesArray.add(new Shapes(16, 16, position, Types.OBSTACLE));
+                } else {
+                    shapesArray.add(new Shapes(16, 16, position, Types.UNDEFINED));
                 }
+                System.out.println("[ "+y +","+ x+" ]");
+                position = new int[]{position[0], position[1] + 16};
             }
-            position = new int[]{0,position[1]+16};
+            position = new int[]{position[0] + 16, 0};
         }
-        this.setSprite(bufferedImage,new double[]{1,1});
+        this.setSprite(bufferedImage, new double[]{1, 1});
         this.setShape((shapesArray.toArray(this.getShape())));
     }
-
 
 
     public Map() throws IOException {
@@ -79,7 +78,7 @@ public class Map extends Entity implements IDrawable, IPlugin {
     @Override
     public void draw(Graphics2D g, JPanel panel, GameData gameData) {
 
-        g.drawImage(getSprite().getImage(),0,0,panel);
+        g.drawImage(getSprite().getImage(), 0, 0, panel);
 
     }
 
@@ -89,7 +88,7 @@ public class Map extends Entity implements IDrawable, IPlugin {
     }
 
     @Override
-    public Entity create(GameData gameData){
+    public Entity create(GameData gameData) {
         Entity newMap = null;
         try {
             newMap = new Map();
