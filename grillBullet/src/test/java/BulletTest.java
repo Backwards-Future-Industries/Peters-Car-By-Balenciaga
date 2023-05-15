2d50544508f19ac642564c282c48d64b547fd5b3
Sindahlof <frsin21@student.sdu.dk>
2023-05-15 12:12:05 +0200

    Made bullets test pass

--- grillBullet/src/test/java/BulletTest.java
+++ grillBullet/src/test/java/BulletTest.java
@@ -17,12 +17,17 @@ class BulletTest {
     @BeforeEach
     void setUp() {
         this.bullet = new Bullet();
+        this.bullet.setDirection(new Vector2D(2,4));
+        this.bullet.setPosition(new int[]{20,50});
+        this.bullet.setHealth(1);
+        this.bullet.setAcceleration(1);
+        this.bullet.setMaxSpeed(3);
     }
 
     @Test
     void testVariables() {
-        assertEquals(this.position, bullet.getPosition());
-        assertEquals(this.direction, bullet.getDirection());
+        assertArrayEquals(this.position, bullet.getPosition());
+        assertArrayEquals(this.direction.getComponents(), bullet.getDirection().getComponents());
         assertEquals(bullet.getHealth(), 1);
         assertEquals(bullet.getAcceleration(), 1);
         assertEquals(bullet.getMaxSpeed(), 3);
