aaedb9f10b192f79c77792bbad5c9fe4226ffe8e
PeterXRiber <perib21@student.sdu.dk>
2023-05-16 10:40:22 +0200

    Created the TileTypeTest class
    I Created the TileTypeTest class

---
+++ map/src/test/java/map/TileTypeTest.java
@@ -0,0 +1,29 @@
+package map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTypeTest {
+
+    private static Color color;
+
+    @BeforeEach
+    void setUp
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
