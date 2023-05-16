1eecd2643b73d61d58ec5fa7264aaf2f9f60dcb5
Sindahlof <frsin21@student.sdu.dk>
2023-05-16 10:26:53 +0200

    SPILocator update again

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -2,4 +2,10 @@
 
 module core {
     requires gameEngine;
+    requires common;
+    requires java.desktop;
+
+    uses interfaces.IPlugin;
+    uses interfaces.IProcessing;
+    uses interfaces.IDrawable;
 }
