c607aa9ebb33f4c4e04846b29ccb62c4510a5947
Emre Eker <emeke21@student.sdu.dk>
2023-05-16 11:28:20 +0200

    added point[] to shape t

--- common/src/main/java/utilities/Shapes.java
+++ common/src/main/java/utilities/Shapes.java
@@ -2,9 +2,13 @@ package utilities;
 
 import abstractClasses.Entity;
 
+import java.awt.*;
+
 public class Shapes {
 
     int[] position;
+
+    Point[] rectangle;
     int height;
     int width;
 
@@ -19,8 +23,12 @@ public class Shapes {
         this.height = height;
         this.position = position;
         this.type = type;
+        this.rectangle = new Point[4];
 
-        setPosition(new int[]{1,1});
+        rectangle[0] = new Point(position[0],position[1]);
+        rectangle[1] = new Point(position[0], width);
+        rectangle[2] = new Point(height,position[1]);
+        rectangle[3] = new Point(width,height);
 
     }
 
@@ -28,8 +36,16 @@ public class Shapes {
 
 
     //
-    public int[] getPosition() {
-        return position;
+    public Point[] getPositions(float rotation) {
+        for(int i = 1; i<rectangle.length; i++){
+            double doubleNewX = rectangle[i].x + Math.cos(rotation) * (rectangle[i].x-rectangle[0].x) - Math.sin(rotation) * (rectangle[i].y-rectangle[0].y);
+            double doubleNewY = rectangle[i].y + Math.sin(rotation) * (rectangle[i].x-rectangle[0].x) - Math.cos(rotation) * (rectangle[i].y-rectangle[0].y);
+            int newX = (int) Math.round(doubleNewX);
+            int newY = (int) Math.round(doubleNewY);
+            rectangle[i].setLocation(new Point(newX,newY));
+            }
+
+        return rectangle;
     }
 
 
@@ -37,21 +53,7 @@ public class Shapes {
         this.position = position;
     }
 
-    public int getHeight() {
-        return height;
-    }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
 
 
 
