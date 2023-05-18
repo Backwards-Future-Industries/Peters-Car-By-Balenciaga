e8d9861f4a7804817ab0188d562c5d6fb65a16b1
OFHansen <ohans21@student.sdu.dk>
2023-05-18 11:24:25 +0200

    Types to Type yet again

--- movement/src/test/java/ConcreteEntity.java
+++ movement/src/test/java/ConcreteEntity.java
@@ -1,12 +1,12 @@
 import abstractClasses.Entity;
-import utilities.Types;
+import utilities.Type;
 
 import java.net.URL;
 
 public class ConcreteEntity extends Entity {
 
     public ConcreteEntity(int health, URL sprite) {
-        super(health, sprite, Types.UNDEFINED);
+        super(health, sprite, Type.UNDEFINED);
     }
 
 }
