0b96a736cdb1994ab45a4922b958359044406dfc
DominicNr2 <dongu21@student.sdu.dk>
2023-05-18 11:53:45 +0200

    refactored bitmap to be less redundant
    eibert er holænder thihi fnis

--- map/src/main/java/map/Bitmap.java
+++ map/src/main/java/map/Bitmap.java
@@ -23,6 +23,12 @@ public class Bitmap {
         this.map = new TileType[bitmap.getWidth()][bitmap.getHeight()];
         this.aiMap = new int[bitmap.getHeight()][bitmap.getWidth()];
         loadMap();
+        for (int i = 0; i < aiMap.length; i++) {
+            for (int j = 0; j < aiMap[i].length; j++) {
+                System.out.print(aiMap[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     private void loadMap(){
@@ -35,31 +41,25 @@ public class Bitmap {
                 int red = (color & 0xff0000) >> 16;
                 TileType tileType = findTile(blue,green,red);
                 map[x][y] = tileType;
-                //aiMap[y][x] = tileTypeToInt(tileType);
+                aiMap[y][x] = tileTypeToInt(tileType);
             }
         }
 
     }
 
-
     private TileType findTile(int blue, int green, int red){
         Color color = Color.CYAN;
-
         if(blue == 0 && green == 255 && red == 0){
             color = Color.GREEN;
         }
-
         if(blue == 255 && green == 255 && red == 255){
             color = Color.WHITE;
         }
-
         if (blue == 0 && green == 0 && red == 255){
             color = Color.RED;
         }
-
         if (blue == 128 && green == 128 && red == 128){
             color = Color.GRAY;
-
         }
         return getColorTileType(color);
     }
@@ -67,17 +67,30 @@ public class Bitmap {
     private TileType getColorTileType(Color color) {
         if (TileType.GRASS.getColor() == color) {
             return TileType.GRASS;
-        } else if (TileType.EARTH.getColor() == color) {
+        }else if (TileType.EARTH.getColor() == color) {
             return TileType.EARTH;
         } else if (TileType.OBSTACLE.getColor() == color) {
             return TileType.OBSTACLE;
-        }
-
-        if (TileType.ROAD.getColor() == color) {
+        } else if (TileType.ROAD.getColor() == color) {
             return TileType.ROAD;
         }
-
         return TileType.BLANK;
+    }
+
+    private int tileTypeToInt(TileType tileType){
+        if(tileType == TileType.GRASS){
+            return 2;
+        }
+        if(tileType == TileType.EARTH){
+            return 1;
+        }
+        if (tileType == TileType.OBSTACLE){
+            return 3;
+        }
+        if (tileType == TileType.ROAD) {
+            return 0;
+        }
+        return -1;
     }
 
     public TileType[][] getMap() {
