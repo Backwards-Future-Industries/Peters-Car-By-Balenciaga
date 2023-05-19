1280bcb4ac53ecd9742ad726eac9799031c7aeca
Sindahlof <frsin21@student.sdu.dk>
2023-05-19 22:01:52 +0200

    Refactored bullet test to adhere to the arrange act and assert principle for testing

--- bullet/src/test/java/BulletTest.java
+++ bullet/src/test/java/BulletTest.java
@@ -1,6 +1,6 @@
 import abstractClasses.Entity;
-import bullet.*;
-import org.junit.jupiter.api.BeforeEach;
+import grillBullet.BulletPlugin;
+import interfaces.IBulletService;
 import org.junit.jupiter.api.Test;
 import utilities.Vector2D;
 
@@ -9,25 +9,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BulletTest {
 
-    private Entity bullet;
-
-    private final int[] position = {20, 50};
-
-    private final Vector2D direction = new Vector2D(2,4);
-    @BeforeEach
-    void setUp() {
-        this.bullet = new Bullet();
-        this.bullet.setDirection(new Vector2D(2,4));
-        this.bullet.setPosition(new int[]{20,50});
-        this.bullet.setHealth(1);
-        this.bullet.setAcceleration(1);
-        this.bullet.setMaxSpeed(3);
-    }
-
     @Test
     void testVariables() {
-        assertArrayEquals(this.position, bullet.getPosition());
-        assertArrayEquals(this.direction.getComponents(), bullet.getDirection().getComponents());
+        //Arrange
+        IBulletService iBulletService = new BulletPlugin();
+        int[] position = {2, 4};
+        Vector2D direction = new Vector2D(20,50);
+
+        //Act
+        Entity bullet = iBulletService.create(new int[]{2,4},2.5);
+        bullet.setHealth(1);
+        bullet.setAcceleration(1);
+        bullet.setMaxSpeed(3);
+        bullet.setDirection(new Vector2D(20,50));
+
+        //Assert
+        assertArrayEquals(position, bullet.getPosition());
+        assertArrayEquals(direction.getComponents(), bullet.getDirection().getComponents());
         assertEquals(bullet.getHealth(), 1);
         assertEquals(bullet.getAcceleration(), 1);
         assertEquals(bullet.getMaxSpeed(), 3);
--- player/src/test/java/PlayerPluginTest.java
+++ player/src/test/java/PlayerPluginTest.java
@@ -12,51 +12,34 @@ import utilities.Vector2D;
 import java.util.LinkedList;
 
 public class PlayerPluginTest {
-    private GameData gameData;
 
-    @BeforeEach
-    void setUp() {
-        this.gameData = new GameData();
-
-
-    }
-
-    @Test
-    public void testPosition() {
-        //Arrange
-        Entity playerPlugin = new Player();
-
-        //Act
-        playerPlugin.setPosition(new int[]{700, 500});
-        int[] position = new int[]{700, 500};
-
-        //Assert
-        Assertions.assertEquals(position[0], playerPlugin.getPosition()[0]);
-        Assertions.assertEquals(position[1], playerPlugin.getPosition()[1]);
-    }
 
     @Test
     public void testCreation() {
         //Arrange
         IPlugin iPlugin = new PlayerPlugin();
+        int[] position = new int[]{700, 500};
 
         //Act
         Entity entity = iPlugin.create();
+        entity.setPosition(new int[]{700, 500});
 
         //Assert
         Assertions.assertEquals(entity.getType(), Type.PLAYER);
         Assertions.assertEquals(entity.getDirection().getClass(), Vector2D.class);
+        Assertions.assertEquals(position[0], entity.getPosition()[0]);
+        Assertions.assertEquals(position[1], entity.getPosition()[1]);
     }
 
     @Test
     public void testDelete() {
         //Arrange
         LinkedList<Entity> entityMap = new LinkedList<>();
-        IPlugin iPlugin = new PlayerPlugin()
-        ;
+        IPlugin iPlugin = new PlayerPlugin();
+        GameData gameData = new GameData();
 
         //Act
-        this.gameData.addNewEntity(iPlugin.create());
+        gameData.addNewEntity(iPlugin.create());
         iPlugin.delete(gameData, gameData.getEntityList(Type.PLAYER).get(0));
 
         //Assert
