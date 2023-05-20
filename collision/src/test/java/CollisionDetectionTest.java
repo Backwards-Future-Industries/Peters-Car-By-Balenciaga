f0769d47ecfd6f2e9538b754837a77f6813ef201
OFHansen <ohans21@student.sdu.dk>
2023-05-20 04:55:48 +0200

    Collision refactor

--- collision/src/test/java/CollisionDetectionTest.java
+++ collision/src/test/java/CollisionDetectionTest.java
@@ -2,25 +2,36 @@ import abstractClasses.Entity;
 import collision.CollisionDetection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import utilities.Type;
+import utilities.image.ImageLoader;
 
+import java.awt.*;
 import java.net.URL;
+import static org.mockito.Mockito.when;
 
 class CollisionDetectionTest {
 
+    @Mock
+    private Entity testEntity1;
+    @Mock
+    private Entity testEntity2;
+
     CollisionDetection collisionDetection = new CollisionDetection();
     //Placeholder is 480x288
-    private static URL sprite = ConcreteEntity.class.getClassLoader().getResource("images/placeholder.png");
-
-    Entity testEntity1;
-    Entity testEntity2;
+    private static URL sprite = CollisionDetectionTest.class.getClassLoader().getResource("images/placeholder.png");
 
     @BeforeEach
     public void setup(){
-        this.testEntity1 = new ConcreteEntity();
-        this.testEntity2 = new ConcreteEntity();
+        //Arrange
+        MockitoAnnotations.initMocks(this);
+        when(this.testEntity1.getSprite()).thenReturn(ImageLoader.loadImage(sprite, new double[]{1, 1}));
+        when(this.testEntity2.getSprite()).thenReturn(ImageLoader.loadImage(sprite, new double[]{1, 1}));
+        when(this.testEntity1.getPosition()).thenReturn(new Point(0, 0));
+
+        //Act
         testEntity1.setHealth(1);
         testEntity2.setHealth(1);
         testEntity1.setSprite(sprite, new double[]{1,1});
@@ -31,18 +42,16 @@ class CollisionDetectionTest {
 
     @Test
     void testCollisionDetection() {
-        testEntity1.setPosition(new int[]{0, 0});
         testEntity1.getSprite().redoSourceRectangle(0,0);
-        testEntity2.setPosition(new int[]{479, 0});
+        when(this.testEntity2.getPosition()).thenReturn(new Point(479, 0));
         testEntity2.getSprite().redoSourceRectangle(479, 0);
         Assertions.assertTrue(collisionDetection.isColliding(testEntity1, testEntity2));
     }
 
     @Test
     void testNoCollision() {
-        testEntity1.setPosition(new int[]{0, 0});
         testEntity1.getSprite().redoSourceRectangle(0,0);
-        testEntity2.setPosition(new int[]{0, 289});
+        when(this.testEntity2.getPosition()).thenReturn(new Point(0, 289));
         testEntity2.getSprite().redoSourceRectangle(0, 289);
         Assertions.assertFalse(collisionDetection.isColliding(testEntity1, testEntity2));
     }
